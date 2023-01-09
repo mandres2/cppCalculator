46bed466437eb2b7df2e9841387734d58beab3d7
Micah Andres <limbo.linux.dev@gmail.com>
2023-01-08 16:14:42 -1000

    added cppCalculator @ 1614 hours | added Calculator.cpp and Calculator.h

---
+++ Calculator.cpp
@@ -0,0 +1,22 @@
+//
+// Created by micah on 08-Jan-23.
+//
+
+#include "Calculator.h"
+
+double Calculator::Calculate(double x, char oper, double y)
+{
+    switch(oper)
+    {
+        case '+':
+            return x + y;
+        case '-':
+            return x - y;
+        case '*':
+            return x * y;
+        case '/':
+            return x / y;
+        default:
+            return 0.0;
+    }
+}
---
+++ Calculator.h
@@ -0,0 +1,10 @@
+//
+// Created by micah on 08-Jan-23.
+//
+#pragma once
+class Calculator
+{
+public:
+    double Calculate(double x, char oper, double y);
+};
+
